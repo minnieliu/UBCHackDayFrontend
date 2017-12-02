ae0de71c4d9a89a5d9c5590246c008c38ac45a05
minnieliu <minnieliu96@hotmail.com>
2017-12-02 15:47:34 -0800

    adding finish and 100% div css

--- front-end/src/03_finish/finish.js
+++ front-end/src/03_finish/finish.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import check from './checks.png';
+import { RaisedButton } from 'material-ui';
 
 class Finish extends Component {
   render() {
     return (
-      <div>
-        Finish Screen
-        <Link to='/'>Back to Start</Link>
+      <div style={styles.container}>
+       <h1>
+           Thank you for your submission!
+           </h1>
+          <img src={check} alt = "check"/>
+          <h2>Check your email to see who you have matched with.</h2>
+          <RaisedButton style={styles.buttonContainer}>
+          <Link to='/'>Back to Start</Link>
+              </RaisedButton>
 
       </div>
     );
@@ -14,3 +22,33 @@ class Finish extends Component {
 }
 
 export default Finish;
+
+const styles = {
+    container: {
+        backgroundColor: '#67c2ff',
+        display: 'flex',
+        flex: 'none',
+        alignItems: 'center',
+        flexFlow: 'column',
+        height: '100%',
+        width: '100%',
+        margin: 0,
+        resizeMode: 'cover',
+        flex: 1,
+
+        color: '#FFF',
+        height: 'min-content',
+        minHeight: 1600,
+        width: '100%',
+        margin: 0
+
+    },
+    buttonContainer: {
+        alignItems: 'baseline',
+        margin: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20
+    }
+
+}
