46e26b122e1b179480dfff3432cc89853505c4e1
John Lee <PiggySpeed@users.noreply.github.com>
2017-12-02 12:25:38 -0800

    more work on survey

--- front-end/src/01_survey/survey.js
+++ front-end/src/01_survey/survey.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { RaisedButton } from 'material-ui';
 
 class Survey extends Component {
   render() {
     return (
-      <div>
-        Survey Screen
-        <Link to='/camera'>Camera</Link>
+      <div style={styles.container}>
+        <div style={styles.scrollContainer}>
+          <h1>Tell us about you!</h1>
+          <RaisedButton>
+            <Link to='/camera'>Camera</Link>
+          </RaisedButton>
+        </div>
       </div>
     );
   }
 }
 
 export default Survey;
+
+const styles = {
+  container: {
+    backgroundColor: '#67c2ff',
+    display: 'flex',
+    alignItems: 'center',
+    flexFlow: 'column',
+    height: '100%'
+  },
+  scrollContainer: {
+    display: 'flex',
+    flex: 'none',
+    flexFlow: 'column',
+    alignItems: 'center',
+    padding: '0 150px',
+
+    color: '#FFF',
+    border: '1px solid yellow',
+    height: 'min-content',
+    minHeight: 1600,
+    width: '100%',
+
+  }
+};
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import routes from './routes';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {routes}
-      </div>
+      <MuiThemeProvider>
+        <div style={styles.container}>
+          {routes}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
 export default App;
+
+const styles = {
+  container: {
+    width: '100vw',
+    height: '100%'
+  }
+};
